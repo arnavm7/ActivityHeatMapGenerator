85c55e7241688442aebb83c77f4a2e99c6666bd8
Vimolicious <39393627+Vimolicious@users.noreply.github.com>
2021-10-23 17:46:55 -0500

    :sparkles: Adds /activity route

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,16 @@ import {
 
 //App Imports
 import Home from './components/Home.jsx'
+import ActivityPage from './components/ActivityPage.jsx'
 import './App.css';
 
 export default function App() {
   return (
       <Router>
         <Switch>
+          <Route path="/activity">
+            <ActivityPage />
+          </Route>
           <Route path="/">
             <Home />
           </Route>
---
+++ src/components/ActivityPage.jsx
@@ -0,0 +1,7 @@
+export default function ActivityPage() {
+    return (
+        <main>
+            <h1>Activity Page</h1>
+        </main>
+    );
+}
