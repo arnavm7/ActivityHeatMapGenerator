26c5dea30661ee2a1ee8a05e0f3c13a9337bb97d
Arnav Mangal <mangal.arnav@gmail.com>
2021-10-23 20:19:30 -0500

    added text field

--- src/components/ActivityPage.jsx
+++ src/components/ActivityPage.jsx
@@ -2,11 +2,13 @@
 import useQuery from '../hooks/useQuery';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import TextField from "@material-ui/core/TextField";
 
 import { useState } from 'react';
 // Activity Component.
 export default function ActivityPage() {
     const query = useQuery();
+    const [name, setName] = useState("");
     const [startDate, setStartDate] = useState(new Date());
     return (
         <main>
@@ -14,6 +16,15 @@ export default function ActivityPage() {
             <h2> What days did you perform {query.get('id')}?</h2>
             <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
 
+            <div style={{marginLeft: "5%",}}>
+                <h2>How many times did you perform {query.get('id')} on this day?</h2>
+            <TextField value={name}
+            label="Enter a number"
+            onChange={(e) => {
+            setName(e.target.value);
+            }}/>
+      <h3>Your performed {query.get('id')} {name} times. </h3>
+    </div>
             
         </main>
     );
